f6a73146b60eddd6c5e147a2ba524c4d20f4a8d0
asep komarudin <pojokcodeid@gmail.com>
2022-12-08 05:40:03 +0700

    update script

--- 02-sintak/js/script.js
+++ 02-sintak/js/script.js
@@ -6,7 +6,7 @@ console.log("Hello World");
  * 3. spasi kosong diabaikan oleh javascript
  * 4. blok script diawali dengan { }
  * 5. javascript case sensitive
- * 6  pembuatan variable tidak boleh mengunakan karaketer khusus (: + )
+ * 6. pembuatan variable tidak boleh mengunakan karaketer khusus (: + )
  *
  */
 
