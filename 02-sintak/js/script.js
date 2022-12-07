9901ef8c0b12ac3d5caba09247085fcfadf315be
asep komarudin <pojokcodeid@gmail.com>
2022-12-07 22:21:39 +0700

    cahapter 2 javascript tutorial

---
+++ 02-sintak/js/script.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+/*
+ * 1. remark multi row
+ * 2. akhir dari statemnet akhiri titik koma
+ * 3. spasi kosong diabaikan oleh javascript
+ * 4. blok script diawali dengan { }
+ * 5. javascript case sensitive
+ * 6  pembuatan variable tidak boleh mengunakan karaketer khusus (: + )
+ *
+ */
+
+// ini remark 1 row
+
+let format = true;
+
+if (format) {
+  console.log("COntoh Saja");
+}
