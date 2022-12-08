a616b7142f70d7a8276945f9b1598efb8abe9ef5
asep komarudin <pojokcodeid@gmail.com>
2022-12-08 21:00:00 +0700

    update latihan 3

---
+++ 03-variable/js/script.js
@@ -0,0 +1,20 @@
+// VARIABLE JAVASCRIPT
+
+//ATURAN MEMBUAT VARIABLE DI JAVASCRIPT
+// 1. case sensitive variable
+// 2. tidak boleh menggunakan karakter khusus
+// 3. tidak boleh mengunakan kata yang digunakan khusus
+// 4. Java script memili variable dinamis
+// 5. jika satu kata gunakan huruf kecl, duakata
+// 6. initialisasi variable
+// 7. Setaiap akhir dari statement / variable akhiri ;
+// 8. Mulai dari ES6 kita bisa menggunaan let
+// 9. membuat variable lebih dari 1
+// 10.perbedaan error undevinie & not definid
+// 11 constanta
+
+const angka = 5;
+
+angka = 2;
+
+console.log(angka);
