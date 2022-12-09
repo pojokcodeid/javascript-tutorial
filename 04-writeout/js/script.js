9efbca3acda1aa71ad3d2a2d87dc54e00e42c930
asep komarudin <pojokcodeid@gmail.com>
2022-12-09 23:02:45 +0700

    update latihan 4

---
+++ 04-writeout/js/script.js
@@ -0,0 +1,13 @@
+console.log("Selamat Datang");
+
+var x = 10;
+var y = 20;
+var sum = x + y;
+console.log(sum);
+
+//alert("Hello World");
+//
+// alert(sum);
+
+document.writeln("Selamat Datang");
+document.writeln(sum);
