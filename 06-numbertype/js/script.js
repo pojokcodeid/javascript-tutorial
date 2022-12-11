3574f0fcac7a45a9c51ef4577c2b8d709f9ded67
asep komarudin <pojokcodeid@gmail.com>
2022-12-11 22:58:17 +0700

    Update Latihan 6

---
+++ 06-numbertype/js/script.js
@@ -0,0 +1,37 @@
+// mulai es 2020 diperkenalkan type data primitif bigint
+
+//bilangan bulat
+let counter = 100;
+console.log(counter);
+
+let num = 071;
+console.log(num);
+
+num = 080;
+console.log(num);
+
+num = 0o71;
+console.log(num);
+
+// num = 0o80;
+console.log(num);
+
+let hex = 0x1a;
+console.log(hex);
+
+let price = 9.99;
+let tex = 0.08;
+let diskon = 0.05;
+
+console.log(diskon);
+
+let amount = 3.14e7;
+amount = 5e-7;
+amount = 0.0000005;
+console.log(amount);
+
+let amount2 = 0.2 + 0.1;
+console.log(amount2);
+
+let bint = 123131231313131n;
+console.log(bint);
