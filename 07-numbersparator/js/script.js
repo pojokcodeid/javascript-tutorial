6e36d33d40a7ee7f5d30a47ddd602d72717bb72c
asep komarudin <pojokcodeid@gmail.com>
2022-12-12 22:06:55 +0700

    update latihan 7

---
+++ 07-numbersparator/js/script.js
@@ -0,0 +1,43 @@
+// number sparator
+let buget = 100000000000;
+console.log(buget);
+buget = 100_000_000_000;
+console.log(buget);
+
+let amount = 120_201_123.05;
+console.log(amount);
+
+let expence = 123_450;
+console.log(expence);
+let fee = 12345_00;
+console.log(fee);
+
+let salary = 0.000_001;
+console.log(salary);
+
+let max = 9_121_121_242_233_232_232_443n;
+console.log(max);
+
+let nibbiner = 0b1011_0101;
+console.log(nibbiner);
+
+let val = 0o1234_5670;
+console.log(val);
+
+let message = 0xd0_e0_f0;
+console.log(message);
+
+let a = 051;
+console.log(a);
+
+let b = 059;
+console.log(b);
+
+let c = 0o51;
+console.log(c);
+
+let e = parseInt("111", 2);
+console.log(e);
+
+let f = 0b111;
+console.log(f);
