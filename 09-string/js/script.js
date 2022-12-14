13abf6264b716631b861e89f53391702302194a6
asep komarudin <pojokcodeid@gmail.com>
2022-12-14 22:13:22 +0700

    latihan 9

---
+++ 09-string/js/script.js
@@ -0,0 +1,35 @@
+// string adalah type data primitif
+let str = "Selamat Datang";
+let pesan = "Jangan Lupa Subscribe";
+console.log(pesan);
+
+let nam = `Pojo0k Code`;
+console.log(nam);
+let msg = `"I'm good". sorry"`;
+console.log(msg);
+
+let nama = "Pojok Code";
+let psn = `HI, I"m ${nama}`;
+console.log(psn);
+
+let pesan1 = "Ini\nadalah\tcontoh 'string\"";
+console.log(pesan1);
+
+pesan1 = "Hai Apa Kabar";
+console.log(pesan1.length);
+console.log(pesan1[0]);
+
+let nmadepan = "Pojok";
+let name1 = nmadepan + " Code";
+console.log(name1);
+
+let nmClass = "btn";
+nmClass += " btn-promary";
+nmClass += " null";
+console.log(nmClass);
+
+let status = false;
+let sts1 = status.toString();
+let back = Boolean(sts1);
+back = Boolean("false");
+console.log(back);
