aabc80b789e57fe93608853b528e563af34ba61e
asep komarudin <pojokcodeid@gmail.com>
2022-12-19 22:08:17 +0700

    latihan 14

---
+++ 14-penugasan/js/script.js
@@ -0,0 +1,18 @@
+// operator penugasan
+let counter = 0;
+counter = counter + 1;
+console.log(counter);
+
+let coun = 0;
+coun += 1;
+console.log(coun);
+
+let a = 10,
+  b = 20,
+  c = 30;
+a = b = c;
+console.log(a);
+
+b = c;
+a = b;
+console.log(a);
