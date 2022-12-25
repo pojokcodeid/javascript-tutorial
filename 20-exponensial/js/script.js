36571bd80cf41dbc9ef838605133a77c5b78121a
asep komarudin <pojokcodeid@gmail.com>
2022-12-26 06:20:49 +0700

    update latihan 20

---
+++ 20-exponensial/js/script.js
@@ -0,0 +1,27 @@
+// exponensial -> oprasi perpangkatan
+// Math.pow(base,exponent)
+// saat ini disediakan opdasi (**)
+// (^)
+let result = Math.pow(2, 2);
+console.log(result);
+result = Math.pow(2, 4);
+console.log(result);
+
+// x**n
+// x base number
+// (**) oprasi exponensial
+// n exmponent
+
+let hasil = 2 ** 2;
+console.log(hasil);
+hasil = 2 ** 4;
+console.log(hasil);
+
+hasil = 2n ** 3n;
+console.log(hasil);
+
+let x = 2;
+x **= 4;
+console.log(x);
+let out = (-2) ** 4;
+console.log(out);
